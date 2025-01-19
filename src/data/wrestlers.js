12c682d1a3a4e0401c8cc90d4f1d6506da814a1a
Luigi Trevisan <checkzin123@gmail.com>
2025-01-19 18:20:10 -0300

    added gender attribute to wrestlersData

--- src/data/wrestlers.js
+++ src/data/wrestlers.js
@@ -6,7 +6,8 @@ export const wrestlersData = [
     isChampion: true,
     championshipTitle: "wXw Unified World Wrestling",
     order: 1,
-    isTagTeam: false
+    isTagTeam: false,
+    gender: "male"
   },
   {
     id: 2,
@@ -15,7 +16,8 @@ export const wrestlersData = [
     isChampion: true,
     championshipTitle: "wXw Blitzkrieg World",
     order: 2,
-    isTagTeam: false
+    isTagTeam: false,
+    gender: "male"
   },
   {
     id: 3,
@@ -24,7 +26,8 @@ export const wrestlersData = [
     isChampion: true,
     championshipTitle: "wXw International",
     order: 3,
-    isTagTeam: false
+    isTagTeam: false,
+    gender: "male"
   },
   {
     id: 4,
@@ -33,7 +36,8 @@ export const wrestlersData = [
     isChampion: true,
     championshipTitle: "wXw World Women's",
     order: 4,
-    isTagTeam: false
+    isTagTeam: false,
+    gender: "female"
   },
   {
     id: 5,
@@ -42,7 +46,8 @@ export const wrestlersData = [
     isChampion: true,
     championshipTitle: "wXw Blitzkrieg Tag Team",
     order: 5,
-    isTagTeam: true
+    isTagTeam: true,
+    gender: "female"
   },
   {
     id: 6,
@@ -51,7 +56,8 @@ export const wrestlersData = [
     isChampion: true,
     championshipTitle: "wXw Blitzkrieg Women's",
     order: 6,
-    isTagTeam: false
+    isTagTeam: false,
+    gender: "female"
   },
   {
     id: 7,
@@ -60,7 +66,8 @@ export const wrestlersData = [
     isChampion: true,
     championshipTitle: "wXw World Tag Team",
     order: 7,
-    isTagTeam: true
+    isTagTeam: true,
+    gender: "male"
   },
   {
     id: 8,
@@ -69,7 +76,8 @@ export const wrestlersData = [
     isChampion: true,
     championshipTitle: "wXw Women's Aufgehender",
     order: 8,
-    isTagTeam: false
+    isTagTeam: false,
+    gender: "female"
   },
   {
     id: 9,
@@ -78,7 +86,8 @@ export const wrestlersData = [
     isChampion: true,
     championshipTitle: "wXw Blitzkrieg Iron",
     order: 9,
-    isTagTeam: false
+    isTagTeam: false,
+    gender: "male"
   },
   {
     id: 10,
@@ -87,566 +96,647 @@ export const wrestlersData = [
     isChampion: true,
     championshipTitle: "wXw Valkyrie",
     order: 10,
-    isTagTeam: false
+    isTagTeam: false,
+    gender: "female"
   },
   {
     id: 11,
     name: "Tay Melo",
     image: "/images/wrestlers/tay-melo.png",
     isChampion: false,
-    order: 11
+    order: 11,
+    gender: "female"
   },
   {
     id: 12,
     name: "Luigi Trevisan",
     image: "/images/wrestlers/luigi-trevisan.png",
     isChampion: false,
-    order: 12
+    order: 12,
+    gender: "male"
   },
   {
     id: 13,
     name: "Arisa Nakajima",
     image: "/images/wrestlers/arisa-nakajima.png",
     isChampion: false,
-    order: 13
+    order: 13,
+    gender: "female"
   },
   {
     id: 14,
     name: "Mandy Sacs",
     image: "/images/wrestlers/mandy-sacs.png",
     isChampion: false,
-    order: 14
+    order: 14,
+    gender: "female"
   },
   {
     id: 15,
     name: "Bandido",
     image: "/images/wrestlers/bandido.png",
     isChampion: false,
-    order: 15
+    order: 15,
+    gender: "male"
   },
   {
     id: 16,
     name: "Toni Storm",
     image: "/images/wrestlers/toni-storm.png",
     isChampion: false,
-    order: 16
+    order: 16,
+    gender: "female"
   },
   {
     id: 17,
     name: "Tori Ortiz",
     image: "/images/wrestlers/tori-ortiz.png",
     isChampion: false,
-    order: 17
+    order: 17,
+    gender: "female"
   },
   {
     id: 18,
     name: "Rey Fénix",
     image: "/images/wrestlers/rey-fenix.png",
     isChampion: false,
-    order: 18
+    order: 18,
+    gender: "male"
   },
   {
     id: 19,
     name: "Romero",
     image: "/images/wrestlers/romero.png",
     isChampion: false,
-    order: 19
+    order: 19,
+    gender: "male"
   },
   {
     id: 20,
     name: "Laura Garcia",
     image: "/images/wrestlers/laura-garcia.png",
     isChampion: false,
-    order: 20
+    order: 20,
+    gender: "female"
   },
   {
     id: 21,
     name: "Mariah May",
     image: "/images/wrestlers/mariah-may.png",
     isChampion: false,
-    order: 21
+    order: 21,
+    gender: "female"
   },
   {
     id: 22,
     name: "Sareee",
     image: "/images/wrestlers/sareee.png",
     isChampion: false,
-    order: 22
+    order: 22,
+    gender: "female"
   },
   {
     id: 23,
     name: "Anna Jay",
     image: "/images/wrestlers/anna-jay.png",
     isChampion: false,
-    order: 23
+    order: 23,
+    gender: "female"
   },
   {
     id: 24,
     name: "Michael Oku",
     image: "/images/wrestlers/michael-oku.png",
     isChampion: false,
-    order: 24
+    order: 24,
+    gender: "male"
   },
   {
     id: 25,
     name: "Robert Dreissker",
     image: "/images/wrestlers/robert-dreissker.png",
     isChampion: false,
-    order: 25
+    order: 25,
+    gender: "male"
   },
   {
     id: 26,
     name: "Hammerstone",
     image: "/images/wrestlers/hammerstone.png",
     isChampion: false,
-    order: 26
+    order: 26,
+    gender: "male"
   },
   {
     id: 27,
     name: "Gigi Dolin",
     image: "/images/wrestlers/gigi-dolin.png",
     isChampion: false,
-    order: 27
+    order: 27,
+    gender: "female"
   },
   {
     id: 28,
     name: "David Finlay",
     image: "/images/wrestlers/david-finlay.png",
     isChampion: false,
-    order: 28
+    order: 28,
+    gender: "male"
   },
   {
     id: 29,
     name: "Mark Davis",
     image: "/images/wrestlers/mark-davis.png",
     isChampion: false,
-    order: 29
+    order: 29,
+    gender: "male"
   },
   {
     id: 30,
     name: "Dominik Mysterio",
     image: "/images/wrestlers/dominik-mysterio.png",
     isChampion: false,
-    order: 30
+    order: 30,
+    gender: "male"
   },
   {
     id: 31,
     name: "Deonna Purrazzo",
     image: "/images/wrestlers/deonna-purrazzo.png",
     isChampion: false,
-    order: 31
+    order: 31,
+    gender: "female"
   },
   {
     id: 32,
     name: "Trick Williams",
     image: "/images/wrestlers/trick-williams.png",
     isChampion: false,
-    order: 32
+    order: 32,
+    gender: "male"
   },
   {
     id: 33,
     name: "KENTA",
     image: "/images/wrestlers/kenta.png",
     isChampion: false,
-    order: 33
+    order: 33,
+    gender: "male"
   },
   {
     id: 34,
     name: "Komander",
     image: "/images/wrestlers/komander.png",
     isChampion: false,
-    order: 34
+    order: 34,
+    gender: "male"
   },
   {
     id: 35,
     name: "Masha Slamovich",
     image: "/images/wrestlers/masha-slamovich.png",
     isChampion: false,
-    order: 35
+    order: 35,
+    gender: "female"
   },
   {
     id: 36,
     name: "Cora Jade",
     image: "/images/wrestlers/cora-jade.png",
     isChampion: false,
-    order: 36
+    order: 36,
+    gender: "female"
   },
   {
     id: 37,
     name: "Aliss Ink",
     image: "/images/wrestlers/aliss-ink.png",
     isChampion: false,
-    order: 37
+    order: 37,
+    gender: "female"
   },
   {
     id: 38,
     name: "Tyler Bate",
     image: "/images/wrestlers/tyler-bate.png",
     isChampion: false,
-    order: 38
+    order: 38,
+    gender: "male"
   },
   {
     id: 39,
     name: "Takuya Nomura",
     image: "/images/wrestlers/takuya-nomura.png",
     isChampion: false,
-    order: 39
+    order: 39,
+    gender: "male"
   },
   {
     id: 40,
     name: "Marcel Barthel",
     image: "/images/wrestlers/marcel-barthel.png",
     isChampion: false,
-    order: 40
+    order: 40,
+    gender: "male"
   },
   {
     id: 41,
     name: "Adam Cole",
     image: "/images/wrestlers/adam-cole.png",
     isChampion: false,
-    order: 41
+    order: 41,
+    gender: "male"
   },
   {
     id: 42,
     name: "Soma Watanabe",
     image: "/images/wrestlers/soma-watanabe.png",
     isChampion: false,
-    order: 42
+    order: 42,
+    gender: "male"
   },
   {
     id: 43,
     name: "AZM",
     image: "/images/wrestlers/azm.png",
     isChampion: false,
-    order: 43
+    order: 43,
+    gender: "female"
   },
   {
     id: 44,
     name: "Ilja Dragunov",
     image: "/images/wrestlers/ilja-dragunov.png",
     isChampion: false,
-    order: 44
+    order: 44,
+    gender: "male"
   },
   {
     id: 45,
     name: "Ori Gold",
     image: "/images/wrestlers/ori-gold.png",
     isChampion: false,
-    order: 45
+    order: 45,
+    gender: "male"
   },
   {
     id: 46,
     name: "Amale",
     image: "/images/wrestlers/amale.png",
     isChampion: false,
-    order: 46
+    order: 46,
+    gender: "female"
   },
   {
     id: 47,
     name: "Pénta El Zero Miedo",
     image: "/images/wrestlers/penta-zero.png",
     isChampion: false,
-    order: 47
+    order: 47,
+    gender: "male"
   },
   {
     id: 48,
     name: "Skye Blue",
     image: "/images/wrestlers/skye-blue.png",
     isChampion: false,
-    order: 48
+    order: 48,
+    gender: "female"
   },
   {
     id: 49,
     name: "Kyle Fletcher",
     image: "/images/wrestlers/kyle-fletcher.png",
     isChampion: false,
-    order: 49
+    order: 49,
+    gender: "male"
   },
   {
     id: 50,
     name: "Michael Mistretta",
     image: "/images/wrestlers/michael-mistretta.png",
     isChampion: false,
-    order: 50
+    order: 50,
+    gender: "male"
   },
   {
     id: 51,
     name: "Deity",
     image: "/images/wrestlers/deity.png",
     isChampion: false,
-    order: 51
+    order: 51,
+    gender: "male"
   },
   {
     id: 52,
     name: "Brutus Creed",
     image: "/images/wrestlers/brutus-creed.png",
     isChampion: false,
-    order: 52
+    order: 52,
+    gender: "male"
   },
   {
     id: 53,
     name: "Hadar Horvitz",
     image: "/images/wrestlers/hadar-horvitz.png",
     isChampion: false,
-    order: 53
+    order: 53,
+    gender: "male"
   },
   {
     id: 54,
     name: "El Generico",
     image: "/images/wrestlers/el-generico.png",
     isChampion: false,
-    order: 54
+    order: 54,
+    gender: "male"
   },
   {
     id: 55,
     name: "Gabe Kidd",
     image: "/images/wrestlers/gabe-kidd.png",
     isChampion: false,
-    order: 55
+    order: 55,
+    gender: "male"
   },
   {
     id: 56,
     name: "Julius Creed",
     image: "/images/wrestlers/julius-creed.png",
     isChampion: false,
-    order: 56
+    order: 56,
+    gender: "male"
   },
   {
     id: 57,
     name: "GUNTHER",
     image: "/images/wrestlers/gunther.png",
     isChampion: false,
-    order: 57
+    order: 57,
+    gender: "male"
   },
   {
     id: 58,
     name: "Tatsuya Hanami",
     image: "/images/wrestlers/tatsuya-hanami.png",
     isChampion: false,
-    order: 58
+    order: 58,
+    gender: "male"
   },
   {
     id: 59,
     name: "Arisu Endo",
     image: "/images/wrestlers/arisu-endo.png",
     isChampion: false,
-    order: 59
+    order: 59,
+    gender: "female"
   },
   {
     id: 60,
     name: "Utami Hayashishita",
     image: "/images/wrestlers/utami-hayashishita.png",
     isChampion: false,
-    order: 60
+    order: 60,
+    gender: "female"
   },
   {
     id: 61,
     name: "Icarus",
     image: "/images/wrestlers/icarus.png",
     isChampion: false,
-    order: 61
+    order: 61,
+    gender: "male"
   },
   {
     id: 62,
     name: "Natalia Markova",
     image: "/images/wrestlers/natalia-markova.png",
     isChampion: false,
-    order: 62
+    order: 62,
+    gender: "female"
   },
   {
     id: 63,
     name: "Maverick",
     image: "/images/wrestlers/maverick.png",
     isChampion: false,
-    order: 63
+    order: 63,
+    gender: "male"
   },
   {
     id: 64,
     name: "Kevin Kaiden",
     image: "/images/wrestlers/kevin-kaiden.png",
     isChampion: false,
-    order: 64
+    order: 64,
+    gender: "male"
   },
   {
     id: 65,
     name: "Sam Della Valle",
     image: "/images/wrestlers/sam-della-valle.png",
     isChampion: false,
-    order: 65
+    order: 65,
+    gender: "male"
   },
   {
     id: 66,
     name: "Joel Vox",
     image: "/images/wrestlers/joel-vox.png",
     isChampion: false,
-    order: 66
+    order: 66,
+    gender: "male"
   },
   {
     id: 67,
     name: "Sorani",
     image: "/images/wrestlers/sorani.png",
     isChampion: false,
-    order: 67
+    order: 67,
+    gender: "female"
   },
   {
     id: 68,
     name: "Andrew Cass",
     image: "/images/wrestlers/andrew-cass.png",
     isChampion: false,
-    order: 68
+    order: 68,
+    gender: "male"
   },
   {
     id: 69,
     name: "Jacob Crane",
     image: "/images/wrestlers/jacob-crane.png",
     isChampion: false,
-    order: 69
+    order: 69,
+    gender: "male"
   },
   {
     id: 70,
     name: "Charli Evans",
     image: "/images/wrestlers/charli-evans.png",
     isChampion: false,
-    order: 70
+    order: 70,
+    gender: "female"
   },
   {
     id: 71,
     name: "Julie",
     image: "/images/wrestlers/julie.png",
     isChampion: false,
-    order: 71
+    order: 71,
+    gender: "female"
   },
   {
     id: 72,
     name: "Malakai Black",
     image: "/images/wrestlers/malakai-black.png",
     isChampion: false,
-    order: 72
+    order: 72,
+    gender: "male"
   },
   {
     id: 73,
     name: "Robin Christopher Fohrwerk",
     image: "/images/wrestlers/robin-fohrwerk.png",
     isChampion: false,
-    order: 73
+    order: 73,
+    gender: "male"
   },
   {
     id: 74,
     name: "Cezar Bononi",
     image: "/images/wrestlers/cezar-bononi.png",
     isChampion: false,
-    order: 74
+    order: 74,
+    gender: "male"
   },
   {
     id: 75,
     name: "Mark Andrews",
     image: "/images/wrestlers/mark-andrews.png",
     isChampion: false,
-    order: 75
+    order: 75,
+    gender: "male"
   },
   {
     id: 76,
     name: "Rising HAYATO",
     image: "/images/wrestlers/rising-hayato.png",
     isChampion: false,
-    order: 76
+    order: 76,
+    gender: "male"
   },
   {
     id: 77,
     name: "Atsuki Aoyagi",
     image: "/images/wrestlers/atsuki-aoyagi.png",
     isChampion: false,
-    order: 77
+    order: 77,
+    gender: "male"
   },
   // {
   //   id: 78,
   //   name: "Gus Lethal",
   //   image: "/images/wrestlers/gus-lethal.png",
   //   isChampion: false,
-  //   order: 78
+  //   order: 78,
+  //   gender: "male"
   // },
   {
     id: 79,
     name: "Bozilla",
     image: "/images/wrestlers/bozilla.png",
     isChampion: false,
-    order: 79
+    order: 79,
+    gender: "male"
   },
   {
     id: 80,
     name: "Lola Vice",
     image: "/images/wrestlers/lola-vice.png",
     isChampion: false,
-    order: 80
+    order: 80,
+    gender: "female"
   },
   {
     id: 81,
     name: "Ron Rubyn",
     image: "/images/wrestlers/ron-rubyn.png",
     isChampion: false,
-    order: 81
+    order: 81,
+    gender: "male"
   },
   // {
   //   id: 82,
   //   name: "Randy Nguyen",
   //   image: "/images/wrestlers/randy-nguyen.png",
   //   isChampion: false,
-  //   order: 82
+  //   order: 82,
+  //   gender: "male"
   // },
   {
     id: 83,
     name: "Arez",
     image: "/images/wrestlers/arez.png",
     isChampion: false,
-    order: 83
+    order: 83,
+    gender: "male"
   },
   // {
   //   id: 84,
   //   name: "Albert Bailey",
   //   image: "/images/wrestlers/albert-bailey.png",
   //   isChampion: false,
-  //   order: 84
+  //   order: 84,
+  //   gender: "male"
   // },
   // {
   //   id: 85,
   //   name: "Jordan Lee",
   //   image: "/images/wrestlers/jordan-lee.png",
   //   isChampion: false,
-  //   order: 85
+  //   order: 85,
+  //   gender: "male"
   // },
   {
     id: 86,
     name: "Big Nik",
     image: "/images/wrestlers/big-nik.png",
     isChampion: false,
-    order: 86
+    order: 86,
+    gender: "male"
   },
   {
     id: 87,
     name: "Sol Ruca",
     image: "/images/wrestlers/sol-ruca.png",
     isChampion: false,
-    order: 87
+    order: 87,
+    gender: "female"
   },
   // {
   //   id: 88,
   //   name: "Lori Nguyen",
   //   image: "/images/wrestlers/lori-nguyen.png",
   //   isChampion: false,
-  //   order: 88
+  //   order: 88,
+  //   gender: "female"
   // },
   // {
   //   id: 89,
   //   name: "Bryan Chapman",
   //   image: "/images/wrestlers/bryan-chapman.png",
   //   isChampion: false,
-  //   order: 89
+  //   order: 89,
+  //   gender: "male"
   // },
   // {
   //   id: 90,
   //   name: "Drake West",
   //   image: "/images/wrestlers/drake-west.png",
   //   isChampion: false,
-  //   order: 90
+  //   order: 90,
+  //   gender: "male"
   // }
 ];
