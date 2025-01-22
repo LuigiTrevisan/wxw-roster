56f55803bd7867a5b25b6e4aa9e7560c69b9a19e
Luigi Trevisan <checkzin123@gmail.com>
2025-01-22 19:41:30 -0300

    added react router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,16 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { WrestlingRoster } from './components/WrestlingRoster'
 import { WrestlerAdmin } from './components/WrestlerAdmin'
 
 function App() {
   return (
-    <WrestlingRoster />
+    <Router>
+      <Routes>
+      <Route path="/" element={<WrestlingRoster />} />
+      <Route path="/admin" element={<WrestlerAdmin />} />
+      </Routes>
+    </Router>
   )
 }
 
